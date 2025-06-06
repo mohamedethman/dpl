9d886f17cdf33d5cbd7945ac60c53d8ccef3cdfb
yehdih2001 <ahmed.yehdih@gmail.com>
2025-06-06 21:37:12 +0000

    Create app-config.service.ts

---
+++ src/app/app-config.service.ts
@@ -0,0 +1,31 @@
+// app-config.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({ providedIn: 'root' })
+export class AppConfigService {
+  private config: any;
+
+  constructor(private http: HttpClient) {}
+
+  loadConfig(): Promise<void> {
+    return this.http
+      .get('/assets/parametres.json')
+      .toPromise()
+      .then(config => {
+        this.config = config;
+      })
+      .catch(() => {
+        this.config = {
+          ip: 'http://localhost',
+          port: '8095',
+          context: '/api'
+        };
+      });
+  }
+
+
+  get Config()() {
+    return this.config;
+  }
+}
